f133b6a927d610a80c00aa660ca364f2b56bfbcc
Luke <luke@iMac.local>
2023-01-17 11:01:48 -0600

    initial commit

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+import { SET_GRIDVIEW } from "../actions";
+
+export const formatPrice = (number) => {
+  const newNumber = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(number / 100);
+  return newNumber;
+};
+
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+  return ["all", ...new Set(unique)];
+};
